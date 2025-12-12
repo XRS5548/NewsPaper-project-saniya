33d8ab322b74872e496eea51f748f2e337e3adf2
XRS5548 <myonlineworking5548@gmail.com>
2025-12-12 18:05:35 +0530

    try 2

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -4,7 +4,8 @@ import Payment from "../pages/Payment.jsx";
 import Newspaper from "../pages/Newspaper.jsx";
 import Agent from "../pages/Agent.jsx";
 import History from "../pages/History.jsx";
-import Profile from '../pages/profile.jsx'
+
+import Profile from "../pages/profile.jsx"
 
 export default function Sidebar() {
     const [activeSection, setActiveSection] = useState("profile");
